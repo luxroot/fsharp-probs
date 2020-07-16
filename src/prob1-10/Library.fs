7fab3b9fe4a2e152939894b54ae0873d7b7b5fc4
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2020-07-17 00:22:27 +0900

    Solved some problems

--- src/prob1-10/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace prob1_10
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/prob1-10/Solution.fs
@@ -0,0 +1,27 @@
+﻿module prob1_10
+
+let rec myLast lst =
+    match lst with
+    | [x] -> x
+    | hd :: tl -> myLast tl
+    | [] -> failwith "Empty array"
+
+let rec myButLast lst =
+    match lst with
+    | [] | [_] -> failwith "Not enough elements"
+    | [x; _] -> x
+    | hd :: tl -> myButLast tl
+
+let rec elementAt lst idx =
+    if idx = 1 then
+        List.head lst
+    else
+        elementAt (List.tail lst) (idx-1)
+
+let rec myLength lst =
+    match lst with
+    | _ :: tl -> 1 + (myLength tl)
+    | [] -> 0
+
+let reverse = List.rev
+
