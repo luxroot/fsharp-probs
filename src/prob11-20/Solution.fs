1062bb6221b449941c441dd4ad6c50754d0ed479
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2020-07-21 18:46:30 +0900

    Solved first problem

--- src/prob11-20/Solution.fs
+++ src/prob11-20/Solution.fs
@@ -1,5 +1,14 @@
-﻿namespace prob11_20
+﻿module prob1_10
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type 'a Encoding = Multiple of int * 'a | Single of 'a
+
+let decodeModified lst =
+    let collecter x = function
+        | [] -> [Single x]
+        | Single item :: tl when x = item -> Multiple (2, item) :: tl
+        | Single item :: tl -> (Single x) :: (Single item) :: tl
+        | Multiple (times, item) :: tl when x = item -> (Multiple (times + 1, item)) :: tl
+        | Multiple (times, item) :: tl -> (Single x) :: (Multiple (times, item)) :: tl
+        | _ -> failwith "Error"
+    List.foldBack collecter lst []
+    
