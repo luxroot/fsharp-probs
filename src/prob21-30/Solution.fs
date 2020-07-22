6b9cba431e5066c48c183ca1f72a657ac4ead8ee
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2020-07-22 17:32:31 +0900

    Starting from 21~

---
+++ src/prob21-30/Solution.fs
@@ -0,0 +1,9 @@
+﻿module ProblemThird
+
+let rec insertAt item lst idx =
+    match idx, lst with
+    | 1, _ -> item :: lst
+    | n, hd :: tl -> hd :: insertAt item tl (n - 1)
+    | _ -> failwith "Error"
+
+let range s e = [ s .. e ]
