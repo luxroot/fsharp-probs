c62b6b6838d0b75e8bc341729a15e6a4b06127d7
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2020-07-22 11:16:53 +0900

    Solved till 15

--- src/prob11-20/Solution.fs
+++ src/prob11-20/Solution.fs
@@ -1,10 +1,10 @@
-﻿module prob1_10
+﻿module prob11_20
 
 type 'a Encoding = Multiple of int * 'a | Single of 'a
 
-let decodeModified lst =
+let encodeModified lst =
     let collecter x = function
-        | [] -> [Single x]
+        | [] -> [ Single x ]
         | Single item :: tl when x = item -> Multiple (2, item) :: tl
         | Single item :: tl -> (Single x) :: (Single item) :: tl
         | Multiple (times, item) :: tl when x = item -> (Multiple (times + 1, item)) :: tl
@@ -12,3 +12,19 @@ let decodeModified lst =
         | _ -> failwith "Error"
     List.foldBack collecter lst []
     
+let decodeModified lst =
+    let unzip = function
+        | Single item -> [ item ]
+        | Multiple (times, item) -> [ for _ in 1 .. times -> item ]
+    List.collect unzip lst
+    
+let encodeDirect = encodeModified
+
+let dupli lst =
+    List.collect (fun x -> [ x; x ]) lst
+    
+let repli lst times =
+    List.collect (fun x -> [ for _ in 1 .. times -> x ]) lst
+    
+let dropEvery lst interval =
+    pass
